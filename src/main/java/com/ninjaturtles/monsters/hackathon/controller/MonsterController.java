d74cdf9f015ada3bdf70537fe79d8500b090e557
Elodie Tran <mcetran@gmail.com>
2019-10-31 12:03:31 +0100

    fix api links

--- src/main/java/com/ninjaturtles/monsters/hackathon/controller/MonsterController.java
+++ src/main/java/com/ninjaturtles/monsters/hackathon/controller/MonsterController.java
@@ -7,7 +7,7 @@ import com.ninjaturtles.monsters.hackathon.model.Monster;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -21,60 +21,67 @@ public class MonsterController {
         return "index";
     }
 
-    @GetMapping("/catalogue")
-    public String catalogue() {
+    @GetMapping("/catalogue/{id}")
+    public String catalogue(Model model, @PathVariable Long id) {
+        Monster monsterObject = getMonsterFromAPI(id);
+        model.addAttribute("monsterInfos", monsterObject);
         return "catalogue";
     }
 
-    @GetMapping("/profile")
-    public String profile() {
+    @GetMapping("/profile/{id}")
+    public String profile(Model model, @PathVariable Long id) {
+        Monster monsterObject = getMonsterFromAPI(id);
+        model.addAttribute("monsterInfos", monsterObject);
         return "profile";
     }
 
-    @GetMapping("/match")
-    public String match() {
+    @GetMapping("/match/{id}")
+    public String match(Model model, @PathVariable Long id) {
+        Monster monsterObject = getMonsterFromAPI(id);
+        model.addAttribute("monsterInfos", monsterObject);
         return "match";
     }
 
-    @GetMapping("/account")
-    public String account() {
+    @GetMapping("/account/{id}")
+    public String account(Model model, @PathVariable Long id) {
+        Monster monsterObject = getMonsterFromAPI(id);
+        model.addAttribute("monsterInfos", monsterObject);
         return "account";
     }
 
-    @GetMapping("/discussion")
-    public String discussion() {
+    @GetMapping("/discussion/{id}")
+    public String discussion(Model model, @PathVariable Long id) {
+        Monster monsterObject = getMonsterFromAPI(id);
+        model.addAttribute("monsterInfos", monsterObject);
         return "discussion";
     }
 
-    @GetMapping("/listmatch")
-    public String listmatch() {
+    @GetMapping("/listmatch/{id}")
+    public String listmatch(Model model, @PathVariable Long id) {
+        Monster monsterObject = getMonsterFromAPI(id);
+        model.addAttribute("monsterInfos", monsterObject);
         return "listmatch";
     }
 
-    @GetMapping("/monsters")
-        public String monster (Model model, @RequestParam Long id) {
+    private Monster getMonsterFromAPI(Long id) {
+        WebClient webClient = WebClient.create(MONSTER_URL);
+        Mono<String> call = webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/monsters/{id}")
+                        .build(id))
+                .retrieve()
+                .bodyToMono(String.class);
 
-            WebClient webClient = WebClient.create(MONSTER_URL);
-            Mono<String> call = webClient.get()
-                    .uri(uriBuilder -> uriBuilder
-                            .path("/monsters/{id}")
-                            .build(id))
-                    .retrieve()
-                    .bodyToMono(String.class);
+        String response = call.block();
 
-            String response = call.block();
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            Monster monsterObject = null;
-            try {
-                JsonNode root = objectMapper.readTree(response);
-                monsterObject = objectMapper.convertValue(root.get("monster"), Monster.class);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-
-            model.addAttribute("monsterInfos", monsterObject);
-
-            return "monsters";
+        ObjectMapper objectMapper = new ObjectMapper();
+        Monster monsterObject = null;
+        try {
+            JsonNode root = objectMapper.readTree(response);
+            monsterObject = objectMapper.convertValue(root.get("monster"), Monster.class);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
         }
+        return monsterObject;
+    }
 }
